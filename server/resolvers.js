9c57211a9863bd048ef808df2e4a929d96b2fc8b
Avas <avas.bajracharya@renegadeinsurance.com>
2024-06-08 13:15:45 +0545

    feat: fetch job by id

--- server/resolvers.js
+++ server/resolvers.js
@@ -3,6 +3,7 @@ import { db } from "./db.js";
 export const resolvers = {
   Query: {
     jobs: () => db.jobs.list(),
+    job: (obj, args) => db.jobs.get(args.id),
   },
   Jobs: {
     company: (job) => db.companies.get(job.companyId),
