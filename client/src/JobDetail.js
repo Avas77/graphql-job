ccaaed646e0b2fe4e2c77acd159fe46c63bfe19e
Avas <avas.bajracharya@renegadeinsurance.com>
2024-06-09 14:22:40 +0545

    feat: fetched graphql request from client

--- client/src/JobDetail.js
+++ client/src/JobDetail.js
@@ -1,24 +1,26 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { jobs } from './fake-data';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { loadJobs } from "./api";
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
-export class JobDetail extends Component {
-  constructor(props) {
-    super(props);
-    const {jobId} = this.props.match.params;
-    this.state = {job: jobs.find((job) => job.id === jobId)};
-  }
+export const JobDetail = () => {
+  const [job, setJob] = useState(null);
+  const { jobId } = useParams();
 
-  render() {
-    const {job} = this.state;
-    return (
-      <div>
-        <h1 className="title">{job.title}</h1>
-        <h2 className="subtitle">
-          <Link to={`/companies/${job.company.id}`}>{job.company.name}</Link>
-        </h2>
-        <div className="box">{job.description}</div>
-      </div>
-    );
+  useEffect(() => {
+    loadJobs(jobId).then((res) => setJob(res.job));
+  }, []);
+
+  if (!job) {
+    return null;
   }
-}
+  return (
+    <div>
+      <h1 className="title">{job.title}</h1>
+      <h2 className="subtitle">
+        <Link to={`/companies/${job.company.id}`}>{job.company.name}</Link>
+      </h2>
+      <div className="box">{job.description}</div>
+    </div>
+  );
+};
